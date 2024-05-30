da89963348db753f345d6e4755256d2a3dda3a0a
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-05-30 19:22:19 +0000

    final changes and passed grade

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  
+
+  # Calculate a square
+  get("/square/new", { :controller => "calculators", :action => "square"})
+
+  get("/square/results", { :controller => "calculators", :action => "square_results"})
+
+  # Calculate a square root
+  get("/square_root/new", { :controller => "calculators", :action => "square_root"})
+
+  get("/square_root/results", { :controller => "calculators", :action => "square_root_results"})
+
+  # Calculate a monthly payment
+  get("/payment/new", { :controller => "calculators", :action => "payment"})
+
+  get("/payment/results", { :controller => "calculators", :action => "payment_results"})
+
+  # Calculate a random number (between user input parameters)
+  get("/random/new", { :controller => "calculators", :action => "random"})
+
+  get("/random/results", { :controller => "calculators", :action => "random_results"})
+
+  # Home page navigation
+  get("/", { :controller => "calculators", :action => "square"})
 end
